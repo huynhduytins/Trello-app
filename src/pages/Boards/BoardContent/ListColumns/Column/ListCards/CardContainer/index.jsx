abff9a65fdaac12120f268d05634f0d986f05f73
huynhduytins <hduytins@gmail.com>
2024-05-20 13:45:48 +0700

    implement card container

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/CardContainer/index.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/CardContainer/index.jsx
@@ -1,6 +1,7 @@
 import AttachmentIcon from '@mui/icons-material/Attachment'
 import ModeCommentIcon from '@mui/icons-material/ModeComment'
 import PeopleIcon from '@mui/icons-material/People'
+import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardActionArea from '@mui/material/CardActionArea'
@@ -21,25 +22,37 @@ const CardContainer = ({
 }) => {
   if (isOpen) {
     return (
-      <Card>
-        <CardActionArea>
-          <CardMedia component="img" height="140" image={img} alt="" />
-          <CardContent sx={{ p: 1.5 }}>
-            <Typography>{title}</Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary" startIcon={<PeopleIcon />}>
-            {contributors}
-          </Button>
-          <Button size="small" color="primary" startIcon={<ModeCommentIcon />}>
-            {comments}
-          </Button>
-          <Button size="small" color="primary" startIcon={<AttachmentIcon />}>
-            {attachments}
-          </Button>
-        </CardActions>
-      </Card>
+      <Box
+        sx={{
+          '& .MuiPaper-root': {
+            overflow: 'hidden'
+          }
+        }}
+      >
+        <Card sx={{ height: `${100}px`, transition: 'height 2s' }}>
+          <CardActionArea>
+            <CardMedia component="img" height="140" image={img} alt="" />
+            <CardContent sx={{ p: 1.5 }}>
+              <Typography>{title}</Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary" startIcon={<PeopleIcon />}>
+              {contributors}
+            </Button>
+            <Button
+              size="small"
+              color="primary"
+              startIcon={<ModeCommentIcon />}
+            >
+              {comments}
+            </Button>
+            <Button size="small" color="primary" startIcon={<AttachmentIcon />}>
+              {attachments}
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
     )
   }
 
